173432c0c0322c6cb5e9c0fac5c795b490c5242d
Evgh <kasperovich.official@gmail.com>
2020-12-16 15:02:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace oop_13
+{
+    class Program
+    {
+        delegate void log(string str);
+        event log Notify = KENLogger.Log;
+
+        class KENLogger
+        {
+            public static void Log(string message)
+            {
+                using(StreamWriter sw = new StreamWriter("LogFile.txt", true))
+                {
+                    sw.Write(DateTime.Now);
+                    sw.Write(' ');
+                    sw.Write(message);
+                    sw.WriteLine();
+                }
+            }
+
+
+
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
